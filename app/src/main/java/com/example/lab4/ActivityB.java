d30da8a6a438920076ecc601c7af5875964c1750
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-19 13:12:29 -0400

    Wrapped up UI constraints on MainActicity

---
+++ app/src/main/java/com/example/lab4/ActivityB.java
@@ -0,0 +1,13 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.b_activity);
+    }
+}
--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button triggerIntentButton;
+    Intent intent;
+    final String url = "http://developer.android.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        triggerIntentButton = (Button) findViewById(R.id.button_triggerIntent);
+        triggerIntentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//                startActivity(intent);
+                intent = new Intent();
+                intent.setAction("myAction");
+                intent.addCategory("myCategory");
+                intent.putExtra("url", url);
+                intent.setData(Uri.parse(url));
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "No Activtiy is matched!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+            }
+        });
     }
 }
