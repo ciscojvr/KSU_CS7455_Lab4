ae76cd8cddd8f179036fae6e037a6a2dc55b2644
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-19 13:13:27 -0400

    Create two additional activities ActivityA and ActivityB. Set up required UI elements and added intent filter to manifest

--- app/src/main/java/com/example/lab4/ActivityA.java
+++ app/src/main/java/com/example/lab4/ActivityA.java
@@ -1,13 +1,34 @@
 package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityA extends AppCompatActivity {
+    Button backButtonA;
+    TextView uri_a_textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_activity);
+
+        String url = getIntent().getStringExtra("url");
+
+        uri_a_textView = (TextView) findViewById(R.id.textView_uri_a);
+        uri_a_textView.setText("URI: " + url + " in Activity A");
+
+        backButtonA = (Button) findViewById(R.id.button_back_a);
+        backButtonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityA.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lab4/ActivityB.java
+++ app/src/main/java/com/example/lab4/ActivityB.java
@@ -1,13 +1,34 @@
 package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityB extends AppCompatActivity {
+    Button backButtonB;
+    TextView uri_b_textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.b_activity);
+
+        String url = getIntent().getStringExtra("url");
+
+        uri_b_textView = (TextView) findViewById(R.id.textView_uri_b);
+        uri_b_textView.setText("URI: " + url + " in Activity B");
+
+        backButtonB = (Button) findViewById(R.id.button_back_b);
+        backButtonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityB.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
